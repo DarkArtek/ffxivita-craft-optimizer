d6c0d67833a1c95391b58a23314397fbfbdf9cdb
DarkArtek <darkartek@gmail.com>
2019-12-09 10:06:58 +0100

    Commit

---
+++ server.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+var http = require('http');
+var connect = require('connect');
+var serveStatic = require('serve-static');
+var morgan = require('morgan');
+var path = require('path');
+
+var app = connect();
+
+app.use(morgan('dev'));
+app.use('/', serveStatic(path.join(__dirname, 'app')));
+
+const port = 8001;
+app.listen(port);
+
+console.log('Server Listening on port', port);
